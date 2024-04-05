a70ad9db68dae2a9fc497b3af6ed7f440d06b01e
Sushree Satarupa <sushreesatarupa.ss@gmail.com>
2024-04-06 01:21:59 +0530

    template node

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/api/csv',(req,res)=>{
+  const data=req.body.data;
+  const metadata=req.body.metadata;
+  const csvfile=processCSV(data,metadata);
+  res.send(csvfile)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+
+const processCSV=(data,metadata)=>{
+  console.log(`data ${data}`);
+  console.log(`metadata ${metadata}`)
+  return "hello world";
+}

ff5d1d11811227fbbd581cef56b3cccdff467916
Sushree Satarupa <sushreesatarupa.ss@gmail.com>
2024-04-06 02:46:07 +0530

    template node

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.post('/api/csv',(req,res)=>{
-  const data=req.body.data;
-  const metadata=req.body.metadata;
-  const csvfile=processCSV(data,metadata);
-  res.send(csvfile)
-})
+app.post("/api/csv", (req, res) => {
+  const data = req.body.data;
+  const metadata = req.body.metadata;
+  const csvfile = processCSV(data, metadata);
+  res.send(csvfile);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
 
-
-const processCSV=(data,metadata)=>{
+const processCSV = (data, metadata) => {
   console.log(`data ${data}`);
-  console.log(`metadata ${metadata}`)
+  console.log(`metadata ${metadata}`);
   return "hello world";
-}
+};
